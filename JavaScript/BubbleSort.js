2e727b6fdafa53634ec45980168d3a1c81b67595
Devina <36676408+Devina@users.noreply.github.com>
2018-05-13 21:04:09 +0100

    JavaScript - Bubble Sort

---
+++ JavaScript/BubbleSort.js
@@ -0,0 +1,21 @@
+/* Bubble Sort Algorithm - Sorting largest to smallest value in input array */
+
+function bubbleSort(array)
+{
+  let swap, n = array.length-1, x = array;
+
+  do {
+    swap = false;
+    for (let i = 0; i < n; i++) {
+      if (x[i] < x[i+1]) {
+        let temp = x[i];
+        x[i] = x[i+1];
+        x[i+1] = temp;
+        swap = true;
+      }
+    }
+    n--;
+  } while(swap);
+
+  return x;
+}
