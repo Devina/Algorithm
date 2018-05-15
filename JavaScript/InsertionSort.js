53a68976165f03b9d2c8beba6066874035f55763
Devina <36676408+Devina@users.noreply.github.com>
2018-05-15 17:19:27 +0100

    JavaScript - Insertion Sort

---
+++ JavaScript/InsertionSort.js
@@ -0,0 +1,13 @@
+/* Insertion Sort Algorithm */
+
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let temp = array[i], j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
